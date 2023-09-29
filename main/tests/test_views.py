8120e3fc45ee6f9fe7b624866000589725dc1c73
LmOpe <lawalmuhammed44@gmail.com>
2023-09-29 22:20:56 +0100

    Create models for managing products

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from main import forms
+
 # Create your tests here.
 class TestPage(TestCase):
     def test_home_page_works(self):
@@ -18,7 +20,7 @@ class TestPage(TestCase):
     def test_contact_us_page_works(self):
         response = self.client.get(reverse('contact_us'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'main/templates/contact_form.html')
+        self.assertTemplateUsed(response, 'contact_form.html')
         self.assertContains(response, 'BookTime')
         self.assertIsInstance(
             response.context['form'], forms.ContactForm
